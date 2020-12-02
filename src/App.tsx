aec2bc7dd70e3f1a5036fa3a91af2745c17e7935
wolfstark <1038999405@qq.com>
2020-12-02 13:43:52 +0800

    feat: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,222 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useMemo } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+// import { Button, TextField } from "@material-ui/core";
+import { Responsive, WidthProvider } from "react-grid-layout";
+
+import { Slate, Editable, withReact } from "slate-react";
+import {
+	Node,
+	Editor,
+	Transforms,
+	Range,
+	Point,
+	createEditor,
+	Element as SlateElement,
+} from "slate";
+import { withHistory } from "slate-history";
+
+const initialValue = [
+	{
+		type: "paragraph",
+		children: [
+			{
+				text:
+					"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi doloremque tempore, enim officiis molestias veniam eaque saepe harum, rem repellendus beatae quas inventore nisi animi maxime, corrupti numquam quam perspiciatis!",
+			},
+		],
+	},
+	// {
+	// 	type: "block-quote",
+	// 	children: [{ text: "A wise quote." }],
+	// },
+	// {
+	// 	type: "paragraph",
+	// 	children: [
+	// 		{
+	// 			text:
+	// 				'Order when you start a line with "## " you get a level-two heading, like this:',
+	// 		},
+	// 	],
+	// },
+	// {
+	// 	type: "heading-two",
+	// 	children: [{ text: "Try it out!" }],
+	// },
+	// {
+	// 	type: "paragraph",
+	// 	children: [
+	// 		{
+	// 			text:
+	// 				'Try it out for yourself! Try starting a new line with ">", "-", or "#"s.',
+	// 		},
+	// 	],
+	// },
+];
+
+const SHORTCUTS = {
+	"*": "list-item",
+	"-": "list-item",
+	"+": "list-item",
+	">": "block-quote",
+	"#": "heading-one",
+	"##": "heading-two",
+	"###": "heading-three",
+	"####": "heading-four",
+	"#####": "heading-five",
+	"######": "heading-six",
+};
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+const withShortcuts = (editor) => {
+	const { deleteBackward, insertText } = editor;
+
+	editor.insertText = (text) => {
+		const { selection } = editor;
+
+		if (text === " " && selection && Range.isCollapsed(selection)) {
+			const { anchor } = selection;
+			const block = Editor.above(editor, {
+				match: (n) => Editor.isBlock(editor, n),
+			});
+			const path = block ? block[1] : [];
+			const start = Editor.start(editor, path);
+			const range = { anchor, focus: start };
+			const beforeText = Editor.string(editor, range);
+			const type = SHORTCUTS[beforeText];
+
+			if (type) {
+				Transforms.select(editor, range);
+				Transforms.delete(editor);
+				const newProperties: Partial<SlateElement> = {
+					type,
+				};
+				Transforms.setNodes(editor, newProperties, {
+					match: (n) => Editor.isBlock(editor, n),
+				});
+
+				if (type === "list-item") {
+					const list = { type: "bulleted-list", children: [] };
+					Transforms.wrapNodes(editor, list, {
+						match: (n) =>
+							!Editor.isEditor(n) &&
+							SlateElement.isElement(n) &&
+							n.type === "list-item",
+					});
+				}
+
+				return;
+			}
+		}
+
+		insertText(text);
+	};
+	editor.deleteBackward = (...args) => {
+		const { selection } = editor;
+
+		if (selection && Range.isCollapsed(selection)) {
+			const match = Editor.above(editor, {
+				match: (n) => Editor.isBlock(editor, n),
+			});
+
+			if (match) {
+				const [block, path] = match;
+				const start = Editor.start(editor, path);
+
+				if (
+					!Editor.isEditor(block) &&
+					SlateElement.isElement(block) &&
+					block.type !== "paragraph" &&
+					Point.equals(selection.anchor, start)
+				) {
+					const newProperties: Partial<SlateElement> = {
+						type: "paragraph",
+					};
+					Transforms.setNodes(editor, newProperties);
+
+					if (block.type === "list-item") {
+						Transforms.unwrapNodes(editor, {
+							match: (n) =>
+								!Editor.isEditor(n) &&
+								SlateElement.isElement(n) &&
+								n.type === "bulleted-list",
+							split: true,
+						});
+					}
+
+					return;
+				}
+			}
+
+			deleteBackward(...args);
+		}
+	};
+
+	return editor;
+};
+const Element = ({ attributes, children, element }) => {
+	switch (element.type) {
+		case "block-quote":
+			return <blockquote {...attributes}>{children}</blockquote>;
+		case "bulleted-list":
+			return <ul {...attributes}>{children}</ul>;
+		case "heading-one":
+			return <h1 {...attributes}>{children}</h1>;
+		case "heading-two":
+			return <h2 {...attributes}>{children}</h2>;
+		case "heading-three":
+			return <h3 {...attributes}>{children}</h3>;
+		case "heading-four":
+			return <h4 {...attributes}>{children}</h4>;
+		case "heading-five":
+			return <h5 {...attributes}>{children}</h5>;
+		case "heading-six":
+			return <h6 {...attributes}>{children}</h6>;
+		case "list-item":
+			return <li {...attributes}>{children}</li>;
+		default:
+			return <p {...attributes}>{children}</p>;
+	}
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// const layouts = getLayoutsFromSomewhere();
+	const [value, setValue] = useState<Node[]>(initialValue);
+	const renderElement = useCallback((props) => <Element {...props} />, []);
+	const editor = useMemo(
+		() => withShortcuts(withReact(withHistory(createEditor()))),
+		[]
+	);
+	// const [value, setValue] = useState(initialState);
+	// const onChange = ({ value }) => {
+	// 	setValue({ value });
+	// };
+
+	return (
+		<div className="App">
+			{/* <ResponsiveGridLayout
+				// width=""
+				className="layout"
+				// layouts={layouts}
+				breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+				cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+			> */}
+			<Slate
+				editor={editor}
+				value={value}
+				onChange={(value) => setValue(value)}
+			>
+				<Editable
+					renderElement={renderElement}
+					placeholder="Write some markdown..."
+					spellCheck={false}
+					autoFocus
+				/>
+			</Slate>
+			{/* </ResponsiveGridLayout> */}
+		</div>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "fontsource-roboto";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+// import "fontsource-roboto/500.css";
+//300,400,500,700
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
