a91082c5683e1f767bece3113cd3bc9197c31453
wolfstark <1038999405@qq.com>
2020-12-18 16:03:08 +0800

    update

--- public/server.js
+++ public/server.js
@@ -24,6 +24,7 @@ app.post("/addHtml", (req, res) => {
 	win.webContents.send("extension-html", req.body.htmlStr);
 
 	ogs({ url: req.body.url }).then((data) => {
+		// TODO: 对于没添加过的网站去爬取内容
 		const { error, result, response } = data;
 		console.log("error:", error); // This is returns true or false. True if there was a error. The error it self is inside the results object.
 		console.log("result:", result); // This contains all of the Open Graph results
